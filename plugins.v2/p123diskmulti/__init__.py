c2bdc090b682774b505730dc8d65b9b4c915d0fc
Rst307 <mcrockets@outlook.com>
2026-08-07 22:50:14 +0800

    修复版本号显示：plugin_version 同步为 1.1.0

--- plugins.v2/p123diskmulti/__init__.py
+++ plugins.v2/p123diskmulti/__init__.py
@@ -40,7 +40,7 @@ class P123DiskMulti(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/DDSRem-Dev/MoviePilot-Plugins/main/icons/P123Disk.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.1.0"
     # 插件作者
     plugin_author = "Rst307"
     # 作者主页
